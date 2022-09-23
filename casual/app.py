c31da253ee871d8dc3a79fad902f4a08992d9b49
jaumet <jaume@nualart.cat>
2022-09-23 21:08:35 +0200

    cleaning

--- casual/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<center>Hello World! Uooohh Mantaapp</center>"
-    if __name__ == "__main__":
-        app.run(debug=True,host="research.nualart.cat")
